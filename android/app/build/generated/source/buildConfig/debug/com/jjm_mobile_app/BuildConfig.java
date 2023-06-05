7bda9985ab518e0334d96ab8024c4168fab08b92
Surya Pratap Yadav <spyadav14699@gmail.com>
2023-06-05 23:22:50 +0530

    updated

---
+++ android/app/build/generated/source/buildConfig/debug/com/jjm_mobile_app/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jjm_mobile_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.jjm_mobile_app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 4004;
+  public static final String VERSION_NAME = "4.0";
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+}
---
+++ android/app/build/generated/source/buildConfig/release/com/jjm_mobile_app/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jjm_mobile_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.jjm_mobile_app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 4004;
+  public static final String VERSION_NAME = "4.0";
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+}
